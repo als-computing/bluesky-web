9d1276934d3faeeb85daf506b9fa423178adadc9
Seij De Leon <seij@Seijs-Laptop.dhcp.lbl.gov>
2024-03-07 14:18:21 -0800

    cleanup endpoint names and types

--- src/server/main.py
+++ src/server/main.py
@@ -6,9 +6,10 @@ from pydantic import BaseModel
 
 from ophyd.signal import EpicsSignal
 m7 = EpicsSignal("IOC:m7", name="m7") # initialize a known connection for testing
-pv_dict = {"IOC:m7": m7} # initalize dictionary to hold all PVs
+device_dict = {"IOC:m7": m7} # initalize dictionary to hold all PVs
+# link for commands for a device: https://nsls-ii.github.io/ophyd/generated/ophyd.device.Device.html#ophyd.device.Device
 
-class PVInstruction(BaseModel):
+class DeviceInstruction(BaseModel):
     pv_prefix: str
     set_value: int
     timeout: int | None = None
@@ -36,14 +37,15 @@ app.add_middleware(
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/pv/{pv_prefix}", status_code=200)
-def read_prefix(pv_prefix, response: Response):
-    if pv_prefix in pv_dict:
-        pv = pv_dict.get(pv_prefix)
-        return pv.read()
+@app.get("/devices", status_code=200)
+def list_devices( response: Response):
+    if len(device_dict) > 0:
+        # send a response including the names of all devices
+        device_dict.keys()
+        return{"Device Prefix List" : list(device_dict.keys())}
     else:
         response.status_code = status.HTTP_404_NOT_FOUND
-        return {"404 Error": "PV " + pv_prefix +" not found"}
+    return {"404 Error": "No devices found"}
 
 @app.post("/pv/initialize/{pv_prefix}", status_code=201)
 def initialize_pv(pv_prefix, response: Response):
@@ -51,16 +53,36 @@ def initialize_pv(pv_prefix, response: Response):
         response.status_code = status.HTTP_409_CONFLICT
         return {"409 Error" : "PV " + pv_prefix + " already exists, duplicate connections not allowed"}
     else:
-        #attempt a connection between the PV, if connection is not made then do not add to dictionary
+        response.status_code = status.HTTP_404_NOT_FOUND
+    return {"404 Error": "Device " + prefix +" not found"}
+
+#change pv to device
+# git remote add upstream to als compute, then just push to that and not personal
+@app.get("/devices/{prefix}/position", status_code=200) #make plural for resources, better to keep resource more clear for what it returns
+def read_device(prefix, response: Response):
+    if prefix in device_dict:
+        device = device_dict.get(prefix)
+        return device.read()
+    else:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"404 Error": "Device " + prefix +" not found"}
+
+@app.post("/devices/{prefix}", status_code=201) #avoid verbs in endpoint names, make resource names plural
+def initialize_device(prefix, response: Response):
+    if prefix in device_dict:
+        response.status_code = status.HTTP_409_CONFLICT # combine the response status code and the message in one line
+        return {"409 Error" : "Device " + prefix + " already exists, duplicate connections not allowed"} #returns 200 status with error
+    else:
+        #attempt a connection between the device, if connection is not made then do not add to dictionary
         ## To DO - check that the string is formatted correctly before attempting connection, could also check client side
-        testSignal = EpicsSignal(pv_prefix, name=pv_prefix) # does not throw error if the pv does not exit. It will throw an error if there are duplicate IP addresses though
+        testSignal = EpicsSignal(prefix, name=prefix) # does not throw error if the device does not exit. It will throw an error if there are duplicate IP addresses though
         try:
             testSignal.describe() #throws timeout error if prefix does not exist
         except Exception as error:
-            print("Error: could not establish initial connection to PV: " + pv_prefix)
+            print("Error: could not establish initial connection to device: " + prefix)
             print(error)
             response.status_code = status.HTTP_408_REQUEST_TIMEOUT
-            return {"408 Error" : "PV " + pv_prefix + " is not connected"}
+            return {"408 Error" : "Device " + prefix + " is not connected"}
 
         pv_dict[pv_prefix] = testSignal
         return {"201" : "PV " + pv_prefix + " is connected"}
@@ -73,15 +95,15 @@ async def move_pv(instruction: PVInstruction, response: Response):
             pv.set(instruction.set_value).wait(timeout=1)
             return{"200" : "Instruction accepted, positioned " + instruction.pv_prefix + " to " + str(instruction.set_value) + "."}
         except Exception as error:
-            print("Error: could not move PV " + instruction.pv_prefix)
+            print("Error: could not move device " + instruction.prefix)
             print(error)
             response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-            return{"500 Error" : "Could not move PV " + instruction.pv_prefix}
+            return{"500 Error" : "Could not move device " + instruction.prefix}
     else:
         response.status_code = status.HTTP_404_NOT_FOUND
-        return {"404 Error": "PV " + instruction.pv_prefix  + " not found"}
+        return {"404 Error": "Device " + instruction.prefix  + " not found"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+#@app.get("/items/{item_id}")
+#def read_item(item_id: int, q: Union[str, None] = None):
+#    return {"item_id": item_id, "q": q}
