238121af1a1be16d2619e1a8b328c03d4a45548e
Seij De Leon <seij@Seijs-Laptop.local>
2024-04-02 16:36:26 -0700

    start refactor so user provides websocket url and pv name

--- src/components/Canvas.jsx
+++ src/components/Canvas.jsx
@@ -12,7 +12,11 @@ export default function Canvas() {
     const [lastUpdate, setLastUpdate] = useState('');
     const [averageFPS, setAverageFPS] = useState('');
     const [websocketCommand, setWebsocketCommand] = useState('Connect');
-    var ctx;
+    const [url, setUrl] = useState('');
+    const [prefix, setPrefix] = useState('');
+
+    var ctx; //predefined here so functions have access to it, defined only after canvas loads in useState
+
     //websocket
     const connection = useRef(null);
     function toggleWebSocket() {
@@ -23,9 +27,9 @@ export default function Canvas() {
 
             //for recording time updates and fps
             var imgCounter = 0; 
-            var startTime = Math.floor(Date.now() / 1000); //seconds
-            var previousTime = Math.floor(Date.now() / 1000); //seconds
-            var currentTime = Math.floor(Date.now() / 1000); //seconds
+            const startTime = Math.round(Date.now() / 1000); //seconds
+            var previousTime = Math.round(Date.now() / 1000); //seconds
+            var currentTime = Math.round(Date.now() / 1000); //seconds
 
             socket.addEventListener("open", event => {
                 socket.send("Connection established");
@@ -33,14 +37,16 @@ export default function Canvas() {
             });
 
             socket.addEventListener("message", event => {
-                console.log("received message from socket");
-                ctx.putImageData(createImgData(ctx, JSON.parse(event.data)), 0, 0);
+                console.log("Received Message at: " + Math.round(Date.now() / 1000) + "s");
+                var eventData = JSON.parse(event.data);
+                ctx.putImageData(createImgData(ctx, eventData.img), 0, 0);
                 imgCounter++;
                 currentTime = Math.round(Date.now() / 1000);
                 setLastUpdate((currentTime - previousTime).toPrecision(2));
-                previousTime = currentTime;
                 setAverageFPS((imgCounter / (currentTime - startTime)).toPrecision(3));
-                console.log("Message from server ", JSON.parse(event.data));
+                console.log("Message from server ", eventData);
+                console.log("Time from server send to display = " + (previousTime - eventData.time));
+                previousTime = currentTime;
             });
 
             connection.current = socket;
@@ -57,7 +63,7 @@ export default function Canvas() {
     //Canvas
     const canvasRef = useRef(null);
     const createImgData = (ctx, rgbArray ) => {
-        let canvasImgData = ctx.createImageData(image.width, image.height);
+        let canvasImgData = ctx.createImageData(256, 256);
         //console.log(JSON.parse(rgbArray));
         //console.log(image.rawData);
         //rgbArray = image.rawData;
@@ -82,8 +88,19 @@ export default function Canvas() {
     }, []);
 
     return (
-        <section className="flex w-full flex-col items-center">
-            <canvas className="m-auto" ref={canvasRef} width={image.width} height={image.height} />
+        <section className="flex w-full items-center max-w-screen-lg m-auto p-4">
+            <form className="flex flex-col">
+                <label className="">
+                    <p className="text-xs">Web Socket URL</p>
+                    <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} className="border-b border-slate-300 border-solid focus:outline-none hover:border-slate-500 focus:border-sky-500 transition-all"/>
+                </label>
+                <label>
+                    <p className="text-xs mt-4">Device Prefix (optional)</p>
+                    <input type="text" value={prefix} onChange={(e) => setPrefix(e.target.value)} className="border-b border-slate-300 border-solid focus:outline-none hover:border-slate-500 focus:border-sky-500 transition-all"/>
+                </label>
+                <button className="rounded-lg w-min px-2 py-1 text-white font-medium bg-sky-500 m-auto mt-6">Connect</button>
+            </form>
+            <canvas className="m-auto" ref={canvasRef} width={256} height={256} />
             <p>Last Update: {lastUpdate}s</p>
             <p>Average fps: {averageFPS}</p>
             <button onClick={toggleWebSocket}> {websocketCommand} </ button>
