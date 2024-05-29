4f02cbe90afd5e108a0672e4da8cdb2cb68ae8b2
Seij De Leon <seij@Seijs-Laptop.local>
2024-05-29 11:14:43 -0700

    add locking functionality to controllers and general styling

--- frontend/src/components/ControllerInterface/ControllerInterface.jsx
+++ frontend/src/components/ControllerInterface/ControllerInterface.jsx
@@ -7,10 +7,10 @@ import Button from '../library/Button';
 
 
 const icons = {
-    leftArrowBox: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+    leftArrowBox: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15m-3 0-3-3m0 0 3-3m-3 3H15" />
                     </svg>,
-    rightArrowBox: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+    rightArrowBox: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="">
             <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 9V5.25A2.25 2.25 0 0 1 10.5 3h6a2.25 2.25 0 0 1 2.25 2.25v13.5A2.25 2.25 0 0 1 16.5 21h-6a2.25 2.25 0 0 1-2.25-2.25V15M12 9l3 3m0 0-3 3m3-3H2.25" />
             </svg>,  
     lightning: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-auto h-auto">
@@ -49,7 +49,8 @@ const icons = {
 
 export default function ControllerInterface( {defaultControllerList=[]} ) {
     const [ devices, setDevices ] = useState({});
-    const [ lockoutList, setLockoutList ] = useState([]);
+    const [ lockoutList, setLockoutList ] = useState([]); //for preventing double clicks
+    const [ lockedControllerList, setLockedControllerList ] = useState([]); //for manually preventing user interaction, set by the user on individual controller modules
     const [ controllerList, setControllerList ] = useState(defaultControllerList); //initialize the devices that will be displayed in the controllers section
     const [ isUpArrowVisible, setIsUpArrowVisible ] = useState(false);
     const [ isDownArrowVisible, setIsDownArrowVisible ] = useState(false);
@@ -78,23 +79,32 @@ export default function ControllerInterface( {defaultControllerList=[]} ) {
         if (e.key === "Enter") {
             setDeviceValue(devices[key], devices[key].value, devices[key].setValue, connection, lockoutList, setLockoutList);
         }
-    }
+    };
 
     const handlePopOutClick = (key) => {
         //accepts a string representing the device key associated with the devices object
         var tempArray;
-        console.log(controllerList);
         if (controllerList.includes(key)) {
-            console.log('contains')
             var index = controllerList.indexOf(key);
             tempArray = controllerList.toSpliced(index, 1);
         } else {
-            console.log('does not contain')
             tempArray = controllerList.slice();
             tempArray.push(key);
         }
         console.log(tempArray)
         setControllerList(tempArray);
+    };
+
+    const handleLockClick = (key) => {
+        var tempArray;
+        if (lockedControllerList.includes(key)) {
+            var index = lockedControllerList.indexOf(key);
+            tempArray = lockedControllerList.toSpliced(index, 1);
+        } else {
+            tempArray = lockedControllerList.slice();
+            tempArray.push(key);
+        }
+        setLockedControllerList(tempArray);
     }
 
     const handleScroll = () => {
@@ -131,74 +141,82 @@ export default function ControllerInterface( {defaultControllerList=[]} ) {
 
     return (
         <section className="w-full border border-solid border-slate-500 rounded-md my-4 h-[80vh] max-h-[750px] flex flex-col overflow-hidden">
-            <h2 className="h-8 text-lg">Controller Interface A</h2>
+            <h2 className="h-8 text-lg text-center font-medium">Controller Interface A</h2>
             <section className="w-full h-full flex">
-                <div className="w-2/3 text-center border">
-                    <h2>Controllers</h2>
-                    <div name="" className="flex flex-wrap justify-center">
+                <div className="w-7/12 text-center h-full">
+                    <h2 className="text-sky-900 text-lg">Controllers</h2>
+                    <ul name="" className="flex flex-wrap justify-center h-[calc(100%-4rem)] overflow-y-auto">
                         {controllerList.map((key) => {
                             if (Object.keys(devices).length > 0) { //prevents error due to devices not being empty during testing 
                                 return (
-                                    <li key={devices[key].id} className="flex flex-col border rounded-sm w-60 h-60 list-none m-4 p-1">
+                                    <li key={devices[key].id} className={`flex flex-col border rounded-md w-60 h-60 list-none m-4 p-1 shadow-sm ${lockedControllerList.includes(key) ? 'bg-slate-100' : 'bg-white'}`}>
                                         <div name="Title Heading" className="h-1/6  flex items-start">
-                                            <div name="Lock Device" className="w-1/6 flex justify-start hover:animate-bounce"><div className="cursor-pointer w-5 flex items-start">{icons.unlocked}</div></div>
+                                            <div name="Lock Device" className="w-1/6 flex justify-start hover:animate-pulse" onClick={() => handleLockClick(key)}><div className="cursor-pointer w-5 flex items-start">{ lockedControllerList.includes(key) ? icons.locked : icons.unlocked}</div></div>
                                             <div name="Name and Connection Status" className="w-4/6 flex justify-center py-3">
                                                 <div className={`w-5  ${devices[key].isConnected ? 'text-amber-500 animate-pulse' : 'text-slate-700'}`}>{icons.lightning}</div>
                                                 <p>{ devices[key].nickname ? devices[key].nickname : devices[key].prefix}</p>
                                             </div>
-                                            <div name="Close Box" className="w-1/6 flex justify-end h-auto"><div className="border cursor-pointer w-5">{icons.minus}</div></div>
+                                            <div name="Close Box" className="w-1/6 flex justify-end h-auto"><div className="border cursor-pointer w-5" onClick={() => handlePopOutClick(key)}>{icons.minus}</div></div>
                                         </div>
                                         <div name="Current Value" className="h-1/6  flex justify-center items-center space-x-1 text-lg"><p>{devices[key].value}</p><p>{devices[key].units}</p></div>
                                         <div name="Jog Heading" className="h-1/6  flex justify-center items-end"> <p>Jog</p></div>
-                                        <div name="Jog Buttons" className="h-1/6  flex justify-center items-start space-x-2">
+                                        <div name="Jog Buttons" className={`h-1/6  flex justify-center items-start space-x-2`}>
                                             <div 
                                                 name="Jog Left Button" 
-                                                className="flex justify-center cursor-pointer hover:text-sky-800" 
+                                                className={`flex justify-center  ${lockedControllerList.includes(key) ? 'cursor-not-allowed text-slate-400' : 'cursor-pointer hover:text-sky-800'}`} 
                                                 onClick={() => setDeviceValue(devices[key], devices[key].value, (parseFloat(devices[key].value) - parseFloat(devices[key].increment)), connection, lockoutList, setLockoutList)}>
                                                     {icons.leftArrow}
                                             </div>
-                                            <input name="Jog Value" className="max-w-16 text-center border-b border-slate-500" type="number" value={devices[key].increment} onChange={(e) => setDevices({...devices, [key]: { ...devices[key], increment: parseFloat(e.target.value)}})} />
+                                            <input name="Jog Value" className="max-w-16 text-center border-b border-slate-500 bg-inherit" type="number" value={devices[key].increment} onChange={(e) => setDevices({...devices, [key]: { ...devices[key], increment: parseFloat(e.target.value)}})} />
                                             <div 
                                                 name="Jog Right Button" 
-                                                className="flex justify-center cursor-pointer hover:text-sky-800" 
+                                                className={`flex justify-center  ${lockedControllerList.includes(key) ? 'cursor-not-allowed text-slate-400' : 'cursor-pointer hover:text-sky-800'}`} 
                                                 onClick={() => setDeviceValue(devices[key], devices[key].value, (parseFloat(devices[key].value) + parseFloat(devices[key].increment)), connection, lockoutList, setLockoutList)}>
                                                     {icons.rightArrow}
                                             </div>
                                         </div>
                                         <div name="Set Heading" className="h-1/6 flex justify-center items-end"><p>Set Absolute Value</p></div>
                                         <div name="Set Buttons / Input" className="h-1/6  flex justify-center items-start">
-                                            <input type="number" value={devices[key].setValue} className={`border-b border-black w-4/12 text-right`} onKeyDown={(e) =>handleKeyPress(e, key)} onChange={(e) => setDevices({...devices, [key]: { ...devices[key], setValue: parseFloat(e.target.value)}})}/>
+                                            <input type="number" value={devices[key].setValue} className={`border-b border-black w-4/12 text-right bg-inherit`} onKeyDown={(e) =>handleKeyPress(e, key)} onChange={(e) => setDevices({...devices, [key]: { ...devices[key], setValue: parseFloat(e.target.value)}})}/>
                                             <p className="px-2">{devices[key].units}</p>
-                                            <Button cb={() => setDeviceValue(devices[key], devices[key].value, devices[key].setValue, connection, lockoutList, setLockoutList)} text="Set" styles="px-[6px] py-[1px] text-sm hover:drop-shadow-md hover:bg-sky-600"/>
+                                            <Button 
+                                                cb={() => setDeviceValue(devices[key], devices[key].value, devices[key].setValue, connection, lockoutList, setLockoutList)} 
+                                                text="Set" 
+                                                styles={`px-[6px] py-[1px] text-sm  ${lockedControllerList.includes(key) ? 'cursor-not-allowed bg-slate-400' : 'cursor-pointer hover:bg-sky-600 hover:drop-shadow-md'}`}
+                                            />
                                         </div>
                                     </li>
                                 )
                             }
                         })}
-                    </div>
+                    </ul>
                 </div>
-                <div className="w-1/3 text-center border h-full">
-                    <h2 className="h-8">Device List</h2>
+                <div className="w-5/12 text-center h-full pr-2">
+                    <h2 className="h-8 text-sky-900 text-lg">Device List</h2>
 
-                    <div className="h-[calc(100%-2rem)] relative" id="pvlistContainer">
+                    <div className="h-[calc(100%-5rem)] relative pb-4" id="pvlistContainer">
                         <div id="upArrow" className={`absolute top-0 w-full h-auto flex justify-center bg-[#ffffff99] transition-all duration-700 z-50 ${isUpArrowVisible ? 'opacity-100' : 'z-0 opacity-0'}`}>{icons.upArrow}</div>
                         <div id="downArrow" className={`absolute bottom-0 w-full flex justify-center bg-[#ffffff99] transition-all duration-700 z-50 ${isDownArrowVisible ? 'opacity-100' : 'z-0 opacity-0'}`}>{icons.downArrow}</div>
                         <ul name="PV List" id="pvList" className="absolute top-0 overflow-y-auto h-full w-full z-20" onScroll={handleScroll}>
                             {Object.keys(devices).map((key) => {
                                 return (
-                                    <li className={`flex border border-slate-500 list-none px-2 space-x-1 ${devices[key].isConnected ? 'text-inherit' : 'text-red-500'}`} key={key}>
-                                        <div className="w-1/12 cursor-pointer" onClick={() => handlePopOutClick(key)}>
-                                            {controllerList.includes(key) ? icons.rightArrowBox : icons.leftArrowBox}  
+                                    <li className={`flex  list-none px-2 ${devices[key].isConnected ? 'text-inherit' : 'text-red-500'}`} key={key}>
+                                        <div className={`w-1/12 cursor-pointer flex items-center ${controllerList.includes(key) ? 'pr-2' : 'pl-2'}`} onClick={() => handlePopOutClick(key)}>
+                                            <div className="w-11/12">
+                                                {controllerList.includes(key) ? icons.rightArrowBox : icons.leftArrowBox}  
+                                            </div>
                                         </div>
-                                        <div className="w-1/2 flex">
-                                            <div className="w-6">{icons.lightning}</div>
-                                            <p className="w-full overflow-x-auto">{devices[key].prefix}</p>
-                                        </div>
-                                        <div className="w-3/12 border ">
-                                            <p className="text-right w-full overflow-x-auto">{devices[key].value}</p>
-                                        </div>
-                                        <div className="w-1/12">
-                                            <p>{devices[key].isConnected ? devices[key].units.substring(0,3) : ''}</p>
+                                        <div className="border-slate-500 border w-11/12 flex py-1">
+                                            <div className="w-7/12 flex items-center pl-1">
+                                                <div className={`w-4 ${devices[key].isConnected ? 'text-amber-500 animate-pulse' : 'text-slate-700'}`}>{icons.lightning}</div>
+                                                <p className="w-full text-left overflow-x-auto">{devices[key].prefix}</p>
+                                            </div>
+                                            <div className="w-3/12">
+                                                <p className="text-right w-full overflow-x-auto">{devices[key].value}</p>
+                                            </div>
+                                            <div className="w-2/12">
+                                                <p className="text-center">{devices[key].isConnected ? devices[key].units.substring(0,3) : ''}</p>
+                                            </div>
                                         </div>
                                     </li>
                                 )
