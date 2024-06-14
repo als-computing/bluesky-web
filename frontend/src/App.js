38c092edb764659a9f52049ba2b795e86f46219b
seijdeleon <seijdeleon@lbl.gov>
2024-06-14 09:14:42 -0700

    provide initial support for ultrawide monitors on BL531 page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,9 @@ import Home from './pages/Home.jsx';
 import Camera from './pages/Camera.jsx';
 import Devices from './pages/Devices.jsx';
 import Api from './pages/Api.jsx';
+import QueueServer from './pages/QueueServer.jsx';
 import BL601 from './pages/BL601.jsx';
+import BL531 from './pages/BL531.jsx';
 import ControllerInterface from './components/ControllerInterface/ControllerInterface.jsx';
 
 import { autoDeviceList } from './data/device_names.js';
@@ -19,23 +21,25 @@ function App() {
 
 
   return (
-    <main className="App bg-white min-h-screen w-full px-12 max-w-screen-2xl m-auto">
+    <main className="App bg-white h-screen px-12 sm:max-w-screen-2xl 3xl:max-w-full m-auto">
       <Header />
-      <div className="flex justify-center">
-        <div className="w-1/6">
+      <div className="flex justify-center h-[calc(100%-4rem)] 3xl:justify-around">
+        <div className="w-1/6 h-full 3xl:w-36">
           <Sidebar />
         </div>
-        <div className="w-4/6">
+        <div className="w-4/6 h-full 3xl:w-full">
           <Routes>
             <Route path="/" element={<Home /> } />
             <Route path="/camera" element={<Camera />} />
             <Route path="/devices" element={<Devices />} />
             <Route path="/api" element={<Api />} />
+            <Route path="/queueserver" element={<QueueServer />} />
             <Route path="/beamlines/BL601" element={<BL601 />} />
+            <Route path="/beamlines/BL531" element={<BL531 />} />
             <Route path="controller" element={<ControllerInterface defaultControllerList={['IOC:m1', 'IOC:m2', 'IOC:m3']}/>} />
           </Routes>      
         </div>
-        <div className="w-1/6"></div>
+        <div className="w-1/6 3xl:w-0"></div>
       </div>
     </main>
   );
---
+++ frontend/src/pages/BL531.jsx
@@ -0,0 +1,33 @@
+import { useState, useRef, useEffect } from 'react';
+import DeviceTable from "../components/Motor/DeviceTable";
+import { autoDeviceList } from "../data/device_names";
+import { startAutomaticSetup } from "../utilities/connectionHelper";
+import ControllerInterface from '../components/ControllerInterface/ControllerInterface';
+
+export default function BL531() {
+    const [devices, setDevices] = useState({});  
+    const connection = useRef(null);
+
+    useEffect(()=> {
+        startAutomaticSetup({devices: autoDeviceList.bl531, setDevices: setDevices, connection: connection});
+    },[])
+
+    return (
+        <main>
+            <section>
+                <h1>Beamline 5.3.1</h1>
+                <p>Beamline description here</p>
+                <p>beamline picture here</p>
+            </section>
+            <div className="flex sm:flex-col 3xl:flex-row 3xl:justify-around 3xl:space-x-10 3xl:pl-10 3xl:flex-wrap">
+                <section className="3xl:w-5/12">
+                    <DeviceTable connection={connection} devices={devices} setDevices={setDevices}/>
+                </section>
+                <section className="3xl:w-5/12">
+                    <ControllerInterface defaultControllerList={['DMC01:A', 'DMC01:B']} deviceList={autoDeviceList.bl531}/>
+                </section>
+            </div>
+        </main>
+       
+    )
+}
