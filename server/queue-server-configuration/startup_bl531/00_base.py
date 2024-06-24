e4d7757546856158c80b7aed333b20d33d8a2e4e
seijdeleon <seijdeleon@lbl.gov>
2024-06-24 14:24:49 -0700

    add startup files for queue server initialization

---
+++ server/queue-server-configuration/startup_bl531/00_base.py
@@ -0,0 +1,47 @@
+
+##!!! Instructions for Including these files !!!##
+""" 
+These startup files are run during initialization of the queue server.
+The files instantiate a RE, which persists after an RE environment is opened.
+Custom devices and plans are defined in these files, which will then be automatically 
+added to the list of available plans/devices by the queue server.
+
+"""
+# Required flags for using these files:
+
+# keep_re
+# startup_dire=path/queue-server-configuration/startup
+
+""" 
+keep-re
+startup_dir=path/queue-server-configuration/startup
+
+"""
+    
+# The queue server must include the "keep_re" parameter which prevents the RE in this startup script from being overwritten
+
+from bluesky import RunEngine
+
+RE = RunEngine({})
+
+
+# Send all metadata/data captured to the BestEffortCallback.
+
+
+from databroker.v2 import temp
+db = temp()
+
+# Insert all metadata/data captured into db.
+RE.subscribe(db.v1.insert)
+
+
+from ophyd.sim import det, motor
+
+from bluesky.plans import count, scan
+
+
+from bluesky.callbacks.best_effort import BestEffortCallback
+bec = BestEffortCallback()
+
+# Send all metadata/data captured to the BestEffortCallback.
+RE.subscribe(bec)
---
+++ server/queue-server-configuration/startup_bl531/01_devices.py
@@ -0,0 +1,28 @@
+from ophyd import Component, Device, EpicsSignal, EpicsSignalRO
+#This method of grouping devices should be used for the final production, but I haven't figured out how to call specific devices using the class structure
+""" class M101(Device):
+    m101_pitch_mm = Component(EpicsSignalRO, 'm101_pitch_mm')
+    m101_bend_um = Component(EpicsSignalRO, 'm101_bend_um')
+
+m101_pitch = M101('bl531_esp300:', name='m101_pitch')
+m101_bend = M101('bl531_esp300:', name='m101_bend') """
+
+#Temporary: Manually add each device as a signal
+
+m101_pitch = EpicsSignal("bl531_esp300:m101_pitch_mm", name="m101_pitch")
+m101_bend = EpicsSignal("bl531_esp300:m101_bend_um", name="m101_bend")
+
+dcm_angle = EpicsSignal("bl531_xps1:mono_angle_deg", name="dcm_angle")
+dcm_height = EpicsSignal("bl531_xps1:mono_height_mm", name="dcm_height")
+
+beamstop_horiz = EpicsSignal("bl531_xps2:beamstop_x_mm", name="beamstop_horizon")
+beamstop_vert = EpicsSignal("bl531_xps2:beamstop_y_mm", name="beamstop_vert")
+
+endstation_slit_inboard = EpicsSignal("DMC02:E", name="endstation_slit_inboard")
+endstation_slit_outboard = EpicsSignal("DMC02:F", name="endstation_slit_outboard")
+endstation_slit_top = EpicsSignal("DMC02:G", name="endstation_slit_top")
+endstation_slit_bottom = EpicsSignal("DMC02:H", name="endstation_slit_bottom")
+harm_slit_inboard = EpicsSignal("DMC01:A", name="harm_slit_inboard")
+harm_slit_outboard = EpicsSignal("DMC01:B", name="harm_slit_outboard")
+harm_slit_top = EpicsSignal("DMC01:C", name="harm_slit_top")
+harm_slit_bottom = EpicsSignal("DMC01:D", name="harm_slit_bottom")
