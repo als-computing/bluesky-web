4f16c9c311ad20401f80d5a5b3bf7c38161bb140
seijdeleon <seijdeleon@lbl.gov>
2024-07-05 14:55:59 -0700

    add transition effects to queue item add table

--- frontend/src/components/QueueServer/QSAddItem.jsx
+++ frontend/src/components/QueueServer/QSAddItem.jsx
@@ -12,7 +12,7 @@ export default function QSAddItem() {
     const [isExpanded, setIsExpanded] = useState(false);
     const [allowedPlans, setAllowedPlans] = useState({});
     const [allowedDevices, setAllowedDevices] = useState({});
-    const [activePlan, setActivePlan] = useState({});
+    const [activePlan, setActivePlan] = useState(false);
 
     const handlePlanResponse = (data) => {
         //process the response and provide an empty
@@ -49,38 +49,43 @@ export default function QSAddItem() {
     }, []);
 
     return (
-        <section className={`${isExpanded ? 'w-full' : 'w-96'} border border-solid rounded-lg shadow-lg transition-width duration-1000`}>
+        <section className={`${isExpanded ? 'w-full' : 'w-96'} border border-solid rounded-lg shadow-lg transition-width ease-in duration-1000`}>
             <header onClick={() => isExpanded ? '' : setIsExpanded(true)} className={`${isExpanded ? 'w-full justify-between' : 'hover:cursor-pointer rounded-b-lg'} bg-[#213149] text-white text-2xl px-12 py-3 rounded-t-lg flex items-center space-x-2 justify-center `}>
                 {isExpanded ? <p></p> : ''}
                 <p>Add Queue Item</p>
                 <div onClick={() => setIsExpanded(!isExpanded)} className="hover:cursor-pointer">{isExpanded ? arrowsPointingIn : arrowsPointingOut}</div>
             </header>
-            <form className={`${isExpanded ? 'opacity-100 h-96' : 'h-0 opacity-0'} h-96 flex transition-all duration-1000`}>
-                <div className="w-2/12 border-slate-300 border-r-2">
-                    <div className="bg-gray-200 h-10 text-center flex justify-around items-center">
-                        <h1>PLAN</h1>
-                        <div>{arrowLongLeft}</div>
+            <form className={`${isExpanded ? 'opacity-100 h-96' : 'h-0 opacity-0'} flex transition-all duration-1000 ease-in`}>
+                <div name="PLAN" className={`${activePlan ? 'w-2/12 border-r-2' : 'w-full border-none'} border-slate-300 `}>
+                    <div className="bg-gray-200 h-10 text-center flex justify-between items-center">
+                        <h1 className="pl-3">PLAN</h1>
+                        <div className={`${activePlan ? 'opacity-100 hover:cursor-pointer hover:text-slate-600' : 'opacity-0'} pr-2`} onClick={() => setActivePlan(false)}>{arrowLongLeft}</div>
                     </div>
-                    <ul className="overflow-auto overflow-y-auto max-h-[21.5rem]">
+                    <ul className={`${activePlan ? '' : ''} ${isExpanded ? 'h-[21.5rem] duration-[1100ms]' : 'h-0 duration-700'} overflow-auto overflow-y-auto transition-all ease-in`}>
                         {Object.keys(allowedPlans).map((plan) => {
                             return (
-                                <li key={plan} className={`hover:cursor-pointer hover:bg-purple-300 px-2 my-2 leading-tight`}>{plan.replaceAll('_', ' ')}</li>
+                                <li key={plan} 
+                                    className={`${activePlan === plan ? 'bg-indigo-200' : ''} hover:cursor-pointer group leading-tight flex`} 
+                                    onClick={() => setActivePlan(plan)}>
+                                        <p className={`${activePlan ? 'w-full': 'w-2/12 border-r-2 border-slate-300'} group-hover:bg-indigo-300 px-2 py-1`}>{plan.replaceAll('_', ' ')}</p>
+                                        <p className={`${activePlan ? 'hidden w-0' : 'w-10/12 pl-4 group-hover:bg-indigo-50'}`}>{allowedPlans[plan].description}</p>
+                                </li>
                             )
                         })}
                     </ul>
                 </div>
-                <div className="w-5/12 border-slate-300 border-r-2">
+                <div name="PARAMETERS" className={`${activePlan ? 'w-5/12 border-r-2' : 'w-0 hidden border-none'} border-slate-300 `}>
                     <div className="bg-gray-200 h-10 text-center flex justify-around items-center">
                         <h1>PARAMETERS</h1>
-                        <div>{arrowRefresh}</div>
+                        <div >{arrowRefresh}</div>
                     </div>
                 </div>
-                <div className="w-3/12 border-slate-300 border-r-2">
+                <div name="REVIEW" className={`${activePlan ? 'w-3/12 border-r-2' : 'w-0 hidden border-none'} border-slate-300 `}>
                     <div className="bg-gray-200 h-10 text-center flex justify-start items-center">
                         <h1 className="pl-8">REVIEW</h1>
                     </div>
                 </div>
-                <div className="w-2/12 border-slate-300 border-r-2">
+                <div name="SUBMIT" className={`${activePlan ? 'w-2/12 border-r-2' : 'w-0 hidden border-none'} border-slate-300 `}>
                     <div className="bg-gray-200 h-10 text-center flex justify-start items-center">
                         <h1 className="pl-8">SUBMIT</h1>
                     </div>
