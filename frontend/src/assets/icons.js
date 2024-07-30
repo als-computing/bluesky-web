f6239b02deeb9160e9ef2dc3e1052d579bbb76ed
seijdeleon <seijdeleon@lbl.gov>
2024-07-30 14:38:22 -0700

    add results display for historical queue items in popup

--- frontend/src/assets/icons.js
+++ frontend/src/assets/icons.js
@@ -25,7 +25,7 @@ const tailwindIcons = {
     circleStack: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
                  </svg>,
-    commandLine: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+    commandLine: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m6.75 7.5 3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0 0 21 18V6a2.25 2.25 0 0 0-2.25-2.25H5.25A2.25 2.25 0 0 0 3 6v12a2.25 2.25 0 0 0 2.25 2.25Z" />
                 </svg>,
     clipboardDocumentList: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
@@ -58,7 +58,8 @@ const tailwindIcons = {
     trash: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=""><path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" /></svg>,
     clock: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=""><path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" /></svg>,
     presentationChart: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=""><path strokeLinecap="round" strokeLinejoin="round" d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5m.75-9 3-3 2.148 2.148A12.061 12.061 0 0 1 16.5 7.605" /></svg>,
-  
+    chatBubbleOvalEllipsisLeft: <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className=""><path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" /></svg>,
+
   
   
       
--- frontend/src/components/QueueServer/QItemPopup.jsx
+++ frontend/src/components/QueueServer/QItemPopup.jsx
@@ -7,6 +7,8 @@ import { Tooltip } from 'react-tooltip';
 import Button from "../library/Button";
 import { deleteQueueItem } from "./apiClient";
 
+import dayjs from "dayjs";
+
 const mockDeleteQueueItemResponse = {
     "success": true,
     "msg": "",
@@ -91,7 +93,7 @@ export default function QItemPopup( {popupItem={}, handleQItemPopupClose=()=>{} 
         )
     };
 
-    const rowContent = [
+    const settings = [
         {
             name:'Parameters',
             icon: tailwindIcons.adjustmentsVertical,
@@ -119,6 +121,57 @@ export default function QItemPopup( {popupItem={}, handleQItemPopupClose=()=>{} 
         },
     ];
 
+    var results = [];
+    if (isHistory) {
+        const result = popupItem.result;
+        results = [
+            {
+                name: 'Status',
+                icon: result.exit_status === 'failed' ? tailwindIcons.exclamationTriangle : tailwindIcons.checkmarkInCircle,
+                content: popupItem.result.exit_status
+            },
+            {
+                name: 'Timeline',
+                icon: tailwindIcons.clock,
+                content:
+                    <span>
+                        <p>Start: {dayjs(result.time_start * 1000).format('MM/DD hh:mm:ss a')}</p>
+                        <p>Finish: {dayjs(result.time_stop * 1000).format('MM/DD hh:mm:ss a')}</p>
+                    </span>
+            },
+            {
+                name: 'UIDs',
+                icon: tailwindIcons.fingerprint,
+                content: 
+                    (result.run_uids.length > 0 || result.scan_ids.length > 0) ?
+                        <div>
+                            <div>{result.run_uids.map((uid) => <p key={uid}>run: {uid}</p>)}</div>
+                            <div>{result.scan_ids.map((uid) => <p key={uid}>scan: {uid}</p>)}</div>
+                        </div>
+                    :
+                        null
+            },
+            {
+                name: 'Message',
+                icon: tailwindIcons.chatBubbleOvalEllipsisLeft,
+                content:
+                    (result.msg.length > 0) ?
+                        <p>{result.msg}</p>
+                    :
+                        null
+            },
+            {
+                name: 'Traceback',
+                icon: tailwindIcons.commandLine,
+                content:
+                    (result.traceback.length > 0) ?
+                        <p className="whitespace-pre-wrap h-fit" style={{'scrollbarColor': 'grey white'}}> {result.traceback}</p>
+                    :
+                        null
+            }
+        ];
+    }
+
     const Row = (name, icon, content) => {
         if (name === 'Parameters') {
             return (
@@ -135,12 +188,12 @@ export default function QItemPopup( {popupItem={}, handleQItemPopupClose=()=>{} 
             )
         }
         return(
-            <div key={name} className="flex items-center">
+            <div key={name} className={`${name === 'Message' || name === 'Traceback' ? 'items-start' : 'items-center'} flex`}>
                 <div className="w-1/6"> 
                     <div id={name+'Tooltip'} className="w-10 text-slate-400 m-auto">{icon}</div>
                     <Tooltip anchorSelect={'#' + name + 'Tooltip'} content={name.replace('_', ' ')} place="left" variant="info"/>
                 </div>
-                <p className="w-4/6">
+                <p className={`${name === 'Traceback' ? 'bg-white p-2 rounded-md border border-slate-200' : ''} w-4/6`}>
                     {content}
                 </p>
                 <div className="w-1/6"></div>
@@ -151,20 +204,36 @@ export default function QItemPopup( {popupItem={}, handleQItemPopupClose=()=>{} 
 
         return (
             <div name="background" className={`absolute top-0 left-0 w-full h-full z-10 ${getPlanColorOpacity(popupItem.name)} flex justify-center items-center ${isDeleteModeVisible ? 'bg-red-600/40' : ''}`}>
-                <div name="main popup" className={`relative ${isHistory ? 'w-[60rem]' : 'w-[30rem]'} h-[30rem] rounded-lg ${isDeleteModeVisible ? deleteBg : 'bg-slate-50'}`}>
+                <div name="main popup" className={`relative ${isHistory ? 'w-[90%]' : 'w-[30rem]'} h-[30rem] rounded-lg ${isDeleteModeVisible ? deleteBg : 'bg-slate-50'}`}>
                     {areResultsVisible ? <DeleteResultPopup response={response} cb={handleCloseResults}/> : ''}
                     {isDeleteModeVisible ? <ConfirmDeleteItemPopup handleCancel={handleCancelDeleteClick} handleDelete={handleConfirmDeleteClick} /> : ''}
-                    <span className={`${getPlanColor(popupItem.name)} flex items-center rounded-t-lg ${isDeleteModeVisible ? 'opacity-20' : ''}`}>
+                    <span name="title" className={`${getPlanColor(popupItem.name)} h-[10%] flex items-center rounded-t-lg ${isDeleteModeVisible ? 'opacity-20' : ''}`}>
                         <p className='w-1/12'></p>
                         <p className={`w-10/12 text-center text-white text-2xl py-1  `}>{popupItem.name}</p>
                         <div className='w-1/12 hover:cursor-pointer' onClick={handleQItemPopupClose}>{tailwindIcons.xCircle}</div>
                     </span>
-                    <section className="overflow-auto flex flex-col space-y-4">
-                       {rowContent.map((row) => Row(row.name, row.icon, row.content) )}
-                        <div className={`flex justify-center `}>
-                            <span className={` ${isDeleteModeVisible ? 'hidden' : ''} hover:cursor-pointer w-12 h-12 hover:text-red-500`} onClick={handleFirstDeleteClick}>{tailwindIcons.trash}</span>
-                        </div>
-                    </section>
+                    <div name="content" className="h-[90%] overflow-auto">
+                        {isHistory ? (
+                            <section name="history results" className="overflow-auto flex flex-col space-y-4">
+                                {results.map((item) => {
+                                    return (
+                                        item.content !== null ?
+                                            Row(item.name, item.icon, item.content)
+                                        :
+                                            ''
+                                    )
+                                })}
+                            </section> 
+                        ) : ( 
+                            ''
+                        )}
+                        <section name="settings" className="overflow-auto flex flex-col space-y-4">
+                        {settings.map((row) => Row(row.name, row.icon, row.content) )}
+                            <div className={`flex justify-center `}>
+                                <span className={` ${isDeleteModeVisible ? 'hidden' : ''} hover:cursor-pointer w-12 h-12 hover:text-red-500`} onClick={handleFirstDeleteClick}>{tailwindIcons.trash}</span>
+                            </div>
+                        </section>
+                    </div>
                 </div>
             </div>
         );
@@ -180,7 +249,7 @@ export default function QItemPopup( {popupItem={}, handleQItemPopupClose=()=>{} 
                         <div className='w-1/12 hover:cursor-pointer' onClick={handleQItemPopupClose}>{tailwindIcons.xCircle}</div>
                     </span>
                     <section className="overflow-auto flex flex-col space-y-4">
-                       {rowContent.map((row) => Row(row.name, row.icon, row.content) )}
+                       {settings.map((row) => Row(row.name, row.icon, row.content) )}
                         <div name="delete button" className={`flex justify-center `}>
                             <span className={`${isDeleteModeVisible ? 'hidden' : ''} hover:cursor-pointer w-12 h-12 hover:text-red-500`} onClick={handleFirstDeleteClick}>{tailwindIcons.trash}</span>
                         </div>
