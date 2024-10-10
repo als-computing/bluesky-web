d5a17336d1a2bd742636c5b66e1da6e29054a5ac
Antoine <awojdyla@lbl.gov>
2024-10-10 16:00:54 -0700

    remove toSpliced in lockout controller

--- frontend/src/utilities/controllerHelper.js
+++ frontend/src/utilities/controllerHelper.js
@@ -1,8 +1,12 @@
 const setDeviceValue = (device, currentValue, newValue, connection, lockoutList=[], setLockoutList=()=>{}) => {
+    console.log('set device value')
+    console.log({device})
     if (isValueInBounds(newValue, device.min, device.max, device.prefix)) {
+        console.log('value in bounds')
         if (isDeviceUnlocked(device, lockoutList)) {
             setLockoutList([...lockoutList, device.prefix]); //prevent user from double clicking in short time period
             try {
+                console.log(JSON.stringify({type: "write", pv: device.prefix, value: newValue}))
                 connection.current.send(JSON.stringify({type: "write", pv: device.prefix, value: newValue}));
             } catch (e) {
                 console.log('Error when attempting to send message to WS in handleIncrementClick with ' + device.prefix);
@@ -18,7 +22,9 @@ const unlockDevice = (prefix, lockoutList, setLockoutList) => {
     //removes the first instance of the device name from the lockoutlist
     let index = lockoutList.indexOf(prefix);
     if (index !== -1) {
-        let tempList = lockoutList.toSpliced(index, 1);
+        let tempList = lockoutList.slice();
+        tempList.splice(index, 1);
+        //let tempList = lockoutList.toSpliced(index, 1);
         setLockoutList(tempList);
     } 
 }
