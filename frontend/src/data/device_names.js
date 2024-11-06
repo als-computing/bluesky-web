8c237c8bd9138fd72664d63782d4d4649a47c60e
Antoine <awojdyla@lbl.gov>
2024-11-06 10:00:17 -0800

    add custom devices BL531 into qserver and react client

--- frontend/src/data/device_names.js
+++ frontend/src/data/device_names.js
@@ -1,40 +1,3 @@
-var autoDeviceList = { //TO DO: put this into the data folder and import it
-    bl531: [        
-        'bl531_esp300:m101_pitch_mm', 
-        'bl531_esp300:m101_bend_um', 
-        'bl531_xps1:mono_angle_deg', 
-        'bl531_xps1:mono_height_mm', 
-        'bl531_xps2:beamstop_x_mm', 
-        'bl531_xps2:beamstop_y_mm', 
-        'DMC02:E', 
-        'DMC02:F', 
-        'DMC02:G',
-        'DMC02:H',
-        'DMC01:A',
-        'DMC01:B',
-        'DMC01:C',
-        'DMC01:D'],
-
-    motorMotorSim: [
-        'IOC:m1',
-        'IOC:m2',
-        'IOC:m3',
-        'IOC:m4'
-    ],
-
-    adSimDetector: [
-        '13SIM1:cam1:AcquireTime',
-        '13SIM1:cam1:AcquirePeriod',
-        '13SIM1:cam1:Gain',
-        '13SIM1:cam1:GainRed',
-        '13SIM1:cam1:GainGreen',
-        '13SIM1:cam1:GainBlue',
-        '13SIM1:cam1:AcquireTime_RBV',
-        '13SIM1:cam1:AcquirePeriod_RBV',
-        '13SIM1:cam1:Gain_RBV',
-    ]
-};
-
 var autoDeviceList = {
     //anything ending in ai-ai is analoge signal, we should put into a strip chart
     //have ability for grouping the same units on the same chart via click and drop
@@ -1084,6 +1047,24 @@ var autoDeviceList = {
             nickname: 'Harmonic Suppressor slits Bottom',
             group: 'Harmonic',
             units: '',
+            min: '',
+            max: '',
+            increment: 0,
+        },
+        {
+            prefix: 'bl531_xps1:saxs_det_distance_mm',
+            nickname: 'Pilatus Longitudinal Position',
+            group: 'Pilatus',
+            units: 'mm',
+            min: '-15',
+            max: '10',
+            increment: 0,
+        },
+        {
+            prefix: 'bl531_xps1:es_height_mm',
+            nickname: 'Detector Wedge',
+            group: 'Detector',
+            units: 'mm',
             min: '',
             max: '',
             increment: 0,
