0046ffd3d85698dd5025829cce34fd6918c46349
seijdeleon <seijdeleon@lbl.gov>
2024-11-08 11:51:39 -0800

    python backend auto decodes array data based on int type

--- python-server/pvws.py
+++ python-server/pvws.py
@@ -10,6 +10,22 @@ from ophyd import EpicsSignalRO
 from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 #from fastapi.testclient import TestClient
 
+dtype_map = {
+    'Int8': np.int8,
+    'UInt8': np.uint8,
+    'Int16': np.int16,
+    'UInt16': np.uint16,
+    'Int32': np.int32,
+    'UInt32': np.uint32,
+    'Int64': np.int64,
+    'UInt64': np.uint64,
+    'Float32': np.float32,
+    'Float64': np.float64 
+}
+
+dataTypeEnumList = [
+
+]
 
 router = APIRouter()
 
@@ -56,12 +72,6 @@ async def websocket_endpoint(websocket: WebSocket, num: int | None = None):
         for id, item in enumerate(settingsList):
             settingsList[id]['pv'] = message.get(item['name'], item['defaultPV'])
 
-        #startX_pv = message.get("startX_pv", "13SIM1:cam1:MinX") 
-        #startY_pv = message.get("startY_pv", "13SIM1:cam1:MinY")
-        #sizeX_pv = message.get( "sizeX_pv", "13SIM1:cam1:SizeX")
-        #sizeY_pv = message.get("sizeY_pv", "13SIM1:cam1:SizeY")
-        #colorMode_pv = message.get("colorMode_pv", "13SIM1:cam1:ColorMode")
-        #dataType_pv = message.get("dataType_pv", "13SIM1:cam1:DataType")
     except Exception as e:
         try:
             await websocket.send_text(json.dumps({'error': e}))
@@ -86,11 +96,17 @@ async def websocket_endpoint(websocket: WebSocket, num: int | None = None):
             await websocket.close()
             return
 
-    #color PV returns a numeric value corresponding to enum text
-    colorModeEnumList = ['Mono', 'RGB1', 'RGB2', 'RGB3'] #typical enum layout
-    if hasattr(settingSignals['colorMode'], 'enum_strs'): #overwrite defaults if enums available
+    #color PV and dataType PV return a numeric value corresponding to enum text
+    colorModeEnumList = ['Mono', 'RGB1', 'RGB2', 'RGB3'] #typical enum layout for color mode pv
+    dataTypeEnumList = ['Int8', 'UInt8', 'Int16', 'UInt16', 'Int32', 'UInt32', 'Int64', 'UInt64', 'Float32', 'Float64'] #typical enum layout for data type pv
+    #Overwrite the defaults
+    if hasattr(settingSignals['colorMode'], 'enum_strs'):
         for index, mode in enumerate(settingSignals['colorMode'].enum_strs):
             colorModeEnumList[index] = mode
+
+    if hasattr(settingSignals['dataType'], 'enum_strs'): 
+        for index, dtype in enumerate(settingSignals['dataType'].enum_strs):
+            dataTypeEnumList[index] = dtype
 
     # Put image array data into buffer
     def array_cb(value, timestamp, **kwargs):
@@ -110,6 +126,8 @@ async def websocket_endpoint(websocket: WebSocket, num: int | None = None):
         try:
             colorModeValue = settingSignals['colorMode'].get()
             colorModeText = colorModeEnumList[colorModeValue]
+            dataTypeValue = settingSignals['dataType'].get()
+            dataTypeText = dataTypeEnumList[dataTypeValue]
 
             #print(f'color mode = {colorMode_signal.get()}')
             if colorModeText == 'Mono' :
@@ -122,7 +140,8 @@ async def websocket_endpoint(websocket: WebSocket, num: int | None = None):
             tempDimensions = {
                 'x': settingSignals['sizeX'].get() - settingSignals['startX'].get(),
                 'y': settingSignals['sizeY'].get() - settingSignals['startY'].get(),
-                'channels': channels
+                'channels': channels,
+                'dataType': dataTypeText
             }
             buffer.put_nowait((None, timestamp, tempDimensions))
         except asyncio.QueueFull:
@@ -142,10 +161,14 @@ async def websocket_endpoint(websocket: WebSocket, num: int | None = None):
         await websocket.close()
         return
 
-    array_signal.subscribe(array_cb)
+    array_signal.subscribe(array_cb) 
+
+    #initialize dimensions prior to first frame decode
+    size_cb(None, None)
 
     #load up a single image (if available) into the buffer
     buffer.put_nowait((array_signal.get(), time.time(), False))
+
 
     try:
         while True:
@@ -158,20 +181,29 @@ async def websocket_endpoint(websocket: WebSocket, num: int | None = None):
                 await websocket.send_text(json.dumps(currentDimensions))
                 continue
 
-            rgb_data = np.array(value, dtype=np.uint8)
+            height, width, channels, dataType = currentDimensions['y'], currentDimensions['x'], currentDimensions['channels'], currentDimensions['dataType']
+            try:
+                array_data = np.array(value, dtype=dtype_map[dataType])
+                
+                #scale down anything above 8 bit to avoid partial image displays on client
+                if dataType != 'UInt8' and dataType !='Int8':
+                    max_val = array_data.max() if array_data.max() > 0 else 1
+                    array_data = (array_data / max_val * 255).astype(np.uint8)
+            except Exception as e:
+                print(f'error decoding array_data: {e}') #may occur to a few frames after data Type change
+                await websocket.send_text(json.dumps({'error': e}))
+                continue
 
-            # Reshape the array into a 3D RGB image
-            if len(rgb_data.shape) == 1:  # Assuming 1D array, reshape as needed for RGB
-                try:
-                    height, width, channels = currentDimensions['y'], currentDimensions['x'], currentDimensions['channels']
-                    if channels <=1:
-                        rgb_data = rgb_data.reshape((height, width))
-                    else:
-                        rgb_data = rgb_data.reshape((height, width, channels))
-                except Exception as e: 
-                    print('Skipping this image, mismatch between array data and pv dimensions')
-                    print(e)
-                    continue
+            # Reshape the array into 2d
+            try:
+                if channels <=1:
+                    array_data = array_data.reshape((height, width))
+                else:
+                    array_data = array_data.reshape((height, width, channels))
+            except Exception as e: 
+                print('Skipping this image, mismatch between array data and pv dimensions') #may occur to a few frames after dimension changes
+                print(e)
+                continue
 
             # Resize the image if it exceeds maximum dimension limits
             try:
@@ -179,14 +211,17 @@ async def websocket_endpoint(websocket: WebSocket, num: int | None = None):
                 if currentDimensions['channels'] == 1:
                     mode = 'L' #8 bit pixel grayscale
                 else:
-                    mode = 'RGB' #3x8 pixel RGB
+                    mode = 'RGB' #3x8 pixel RGB #we need more options including rgb1, rgb2, rgb3
+
                 #print('mode selected: ' + mode)
-                if rgb_data.shape[0] > max_dimension or rgb_data.shape[1] > max_dimension:
+                if array_data.shape[0] > max_dimension or array_data.shape[1] > max_dimension:
                     print("Resizing the image to fit within the limit.")
-                    new_size = (min(rgb_data.shape[1], max_dimension), min(rgb_data.shape[0], max_dimension))
-                    img = Image.fromarray(rgb_data, mode).resize(new_size, Image.ANTIALIAS)
+                    new_size = (min(array_data.shape[1], max_dimension), min(array_data.shape[0], max_dimension))
+                    img = Image.fromarray(array_data, mode).resize(new_size, Image.ANTIALIAS)
                 else:
-                    img = Image.fromarray(rgb_data, mode)
+                    img = Image.fromarray(array_data, mode)
+
+                
 
                 buffered = io.BytesIO()
                 img.save(buffered, format="JPEG", quality=100)
