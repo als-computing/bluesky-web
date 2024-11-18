edeb336484ce27170fa265b02b8d8c1830ec3cd6
seijdeleon <seijdeleon@lbl.gov>
2024-11-18 13:29:04 -0800

    add homepage route for proxy testing, add env for prox IP address

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,18 +1,118 @@
-import { useEffect } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
+import Postman from '../components/Postman/Postman';
+import axios from 'axios';
 
 export default function Home() {
-    const navigate = useNavigate();
+    // const navigate = useNavigate();
 
-    useEffect(() => {
+/*     useEffect(() => {
         // Redirect to /queueserver when the component loads
         setTimeout(()=> navigate('/queueserver'), 2000);
         //navigate('/queueserver');
     }, [navigate]);
+ */
+
+    const blankResponseData = [
+        {url: 'http://127.0.0.1:8000', response: ''}, 
+        {url: 'api/python', response: ''}
+    ];
+
+    const wsUrl = 'ws://127.0.0.1:8000/ws';
+
+    const [ responseData, setResponseData ] = useState(blankResponseData);
+    const [ wsMessage, setWsMessage ] = useState('');
+    const ws = useRef(null);
+
+    const Row = ({url, response}) => {
+
+
+        return (
+            <li className="flex">
+                <p className="basis-2/5">
+                    {url}
+                </p>
+                <pre className="basis-3/5 text-wrap text-sm max-h-64 overflow-auto">
+                    {response}
+                </pre>
+            </li>
+        )
+    };
+
+    const getResponse = async (url) => {
+        try {
+            const response = await axios.get(url);
+            return (response);
+        } catch (error) {
+            console.log('Error', error);
+            return (error);
+        }
+    };
+
+    const fetchData = async () => {
+        //loop through and fill out data
+        var tempArray = [];
+        for (let i = 0; i < responseData.length; i++) {
+            //
+            const url = responseData[i].url;
+            const response = await getResponse(url);
+            var tempObject = {url: url, response: JSON.stringify(response, null, 2)};
+            tempArray[i] = tempObject;
+        }
+        setResponseData(tempArray);
+    };
+
+    const startWebSocket = () => {
+    
+        try {
+            ws.current = new WebSocket(wsUrl);
+            //ws.current = new WebSocket('ws://localhost/api/camera');
+        } catch (error) {
+            console.log({error});
+            setWsMessage(JSON.stringify(error, null, 2));
+            return;
+        }
+        ws.current.onopen = (event) => {
+            if (ws.current.readyState === 1) {
+                ws.current.send('testing');
+            }
+        }
+    
+        ws.current.onmessage = async function (event) {
+            setWsMessage(JSON.stringify(event.data, null, 2));
+            console.log(event)
+            console.log(event.data);
+        };
+    
+        ws.current.onerror = (error) => {
+            console.log("WebSocket Error:", error);
+            setWsMessage(JSON.stringify(error, null, 2));
+
+        };
+
+        ws.current.onclose = () => {
+            console.log("Web Socket closed");
+        }
+    };
+
+    useEffect(()=> {
+        fetchData();
+        startWebSocket();
+    }, [])
+
 
     return (
-        <section className='w-full h-40 flex items-center justify-center'>
-            <p>Nothing to see here yet, redirecting to Queue Server</p>
+        <section>
+            <ul className="border border-gray-400 max-w-5xl m-auto flex-col space-y-4 p-2">
+                <Row url='Request URL' response='Response' id='response'/>
+                {responseData.map((responseEntry) => <Row url={responseEntry.url} response={responseEntry.response} key={responseEntry.url}/>)}
+            </ul>
+            <div className="flex">
+                <p className='basis-2/5'>Websocket Messages from {wsUrl}</p>
+                <pre className="bases-3/5 text-wrap text-sm h-64">{wsMessage}</pre>
+            </div>
+
+            <Postman />
         </section>
     )
 }
