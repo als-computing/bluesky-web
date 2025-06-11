064446ccb622f1eb3d3d708d0170fb4f69ec53ab
Nicholas M <nmanha@lbl.gov>
2025-06-11 14:28:35 -0700

    arranged components

--- frontend/src/app/pages/ControlPage.tsx
+++ frontend/src/app/pages/ControlPage.tsx
@@ -1,18 +1,45 @@
 import { useMemo } from 'react';
 
-import {CameraContainer, DeviceControllerBox, Paper, Bento, useOphydSocket} from '@blueskyproject/finch'
+import { CameraContainer, DeviceControllerBox, Paper, Bento, useOphydSocket, TableDeviceController } from '@blueskyproject/finch'
 import { deviceIcons } from '../../assets/icons';
 
 
 export default function BoltControl() {
-    const wsUrl = useMemo(()=>'ws://localhost:8000/ophydSocket', []);
-    const deviceNameList = useMemo(()=>['bl531_esp300:m101_pitch_mm', 'bl531_esp300:m101_bend_um'], []);
+    const wsUrl = useMemo(() => 'ws://localhost:8000/ophydSocket', []);
+    const deviceNameList = useMemo(() => ['13SIM1:m101_pitch_mm', '13SIM1:m101_bend_um'], []);
     const { devices, handleSetValueRequest, toggleDeviceLock, toggleExpand } = useOphydSocket(wsUrl, deviceNameList);
 
     return (
-        <Bento>
-
-            <div className="flex flex-col space-y-8 flex-shrink-0 h-full justify-start">
+        <Bento className="h-full">
+            <div className="flex flex-col justify-evenly h-full">
+                <div className="text-black flex justify-evenly w-full h-1/3 p-2">
+                    <div className="bg-gray-100 w-full flex justify-center"><p>STRIPCHART</p></div>
+                    <div className="bg-gray-100 w-full flex justify-center"><p>STRIPCHART</p></div>
+                    <div className="bg-gray-100 w-full flex justify-center"><p>STRIPCHART</p></div>
+                </div>
+                <div className="flex text-black h-full h-2/3">
+                    <div className="p-2">
+                        <TableDeviceController devices={devices} handleSetValueRequest={handleSetValueRequest} toggleDeviceLock={toggleDeviceLock} toggleExpand={toggleExpand} />
+                    </div>
+                    <div className="flex flex-col p-2">
+                        <DeviceControllerBox
+                            device={devices['13SIM1:m101_pitch_mm']}
+                            handleSetValueRequest={handleSetValueRequest}
+                            handleLockClick={toggleDeviceLock}
+                            svgIcon={deviceIcons.stepperMotor}
+                            className="shadow-xl mb-2"
+                        />
+                        <DeviceControllerBox
+                            device={devices['13SIM1:m101_pitch_mm']}
+                            handleSetValueRequest={handleSetValueRequest}
+                            handleLockClick={toggleDeviceLock}
+                            svgIcon={deviceIcons.stepperMotor}
+                            className="shadow-xl"
+                        />
+                    </div>
+                </div>
+            </div>
+            {/* <div className="flex flex-col space-y-8 flex-shrink-0 h-full justify-start">
                 <DeviceControllerBox 
                     device={devices['bl531_esp300:m101_pitch_mm']} 
                     handleSetValueRequest={handleSetValueRequest} 
@@ -30,7 +57,7 @@ export default function BoltControl() {
             </div>
             <Paper size='large' title="Camera" className='h-full flex-grow'>
                 <CameraContainer prefix="13ARV1" enableControlPanel={true} enableSettings={true} canvasSize="medium" customSetup={false}/>
-            </Paper>
+            </Paper> */}
         </Bento>
 
     )
