0602e4ba21bf216c940a5474086f762ea21984bd
seijdeleon <seijdeleon@lbl.gov>
2026-08-24 11:58:15 -0700

    remove all routes and sockets, revise readme

--- frontend-api/app.py
+++ frontend-api/app.py
@@ -1,20 +1,8 @@
 from typing import Union
 
-from fastapi import FastAPI, Response, status, Request, WebSocket, HTTPException
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-
-
-import tiffStreamer
-
-
-# link for commands for a device: https://nsls-ii.github.io/ophyd/generated/ophyd.device.Device.html#ophyd.device.Device
-
-class DeviceInstruction(BaseModel):
-    pv_prefix: str
-    set_value: int
-    timeout: int | None = None
-
 
 app = FastAPI()
 
@@ -35,11 +23,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-#app.include_router(pvCamera.router) #turn this off if not connected to EPICS
-#app.include_router(pvsim.router)
-#app.include_router(pyfai.router)
-app.include_router(tiffStreamer.router)
 
 @app.get("/")
 def read_root():
